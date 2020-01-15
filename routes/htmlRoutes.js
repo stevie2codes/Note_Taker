eb9231b4cde2695b7aa1f308ac05ba961aa1a482
stevie2codes <stevie2codes@gmail.com>
2020-01-15 17:28:57 -0600

    created new route for index.html page

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,5 +3,9 @@ const path = require('path');
 module.exports = function(app){
     app.get("/notes", function(req, res){
         res.sendFile(path.join(__dirname, "../public/notes.html"))
+    });
+
+    app.get("*", function(req,res){
+        res.sendFile(path.join(__dirname, "../public/index.html"));
     })
 }

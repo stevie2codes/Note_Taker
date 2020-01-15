c45afccb9d49f259d4cb326e66bfe7d5827bbd97
stevie2codes <stevie2codes@gmail.com>
2020-01-15 17:24:40 -0600

    created routes folder with api and html route js files , added an export route function to populate the note page

--- server.js
+++ server.js
@@ -6,10 +6,7 @@ PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('/', function(req, res){
-    res.send("Hello Home page");
-});
 
- 
+require("./routes/htmlRoutes")(app);
 
 app.listen(PORT, () => console.log(`Listening at port ${PORT}`));

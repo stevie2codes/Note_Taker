c5a649a290cce217e5a68846be605f93e3e40e60
stevie2codes <stevie2codes@gmail.com>
2020-01-16 15:03:25 -0600

    added some fs logic to the apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,15 +2,33 @@ const fs = require('fs');
 
 const db = require('../db/db.json');
 
-console.log(db);
+// console.log(db);
 
 module.exports = function(app){
-    app.get("/api/notes", function(req, res){
-        res.json(db);
+    app.get("/api/notes", function(req, res){ 
+       fs.readFile("db/db.json", (err, data) => {
+           if(err) throw err;
+           res.json(data);
+       })
+       res.json(data);
     });
 
     app.post("/api/notes", function(req, res){
-        db.push(req.body)
+       let userNote = req.body;
+       console.log(userNote);
+
+       fs.readFile("db/db.json", (err, data) => {
+           if(err) throw err;
+           let newData = JSON.parse(data);
+           console.log(newData);
+           fs.writeFile("db/db.json", JSON.stringify(userNote, null, 3),() => {
+               console.log("success");
+             
+           })
+
+       })
+
+       res.json(userNote);
         
     })
 }
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,6 +5,14 @@ module.exports = function(app){
         res.sendFile(path.join(__dirname, "../public/notes.html"))
     });
 
+    app.get("/assets/js/index.js", function(req, res){
+        res.sendFile(path.join(__dirname, "../public/assets/js/index.js")); 
+    });
+
+    app.get("/assets/css/styles.css", function(req, res){
+        res.sendFile(path.join(__dirname, "../public/assets/css/styles.css")); 
+    });
+
     app.get("*", function(req,res){
         res.sendFile(path.join(__dirname, "../public/index.html"));
     })

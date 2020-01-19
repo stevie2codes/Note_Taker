0dba6ac2ee5c8de34ef1f95f7f654ba0b7d8c3bc
stevie2codes <stevie2codes@gmail.com>
2020-01-19 12:46:43 -0600

    cleaning up console logs and format

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,39 +1,30 @@
 const fs = require('fs');
 
+module.exports = function (app) {
 
-
-
-// console.log(db);
-
-module.exports = function(app){
-    
     app.get("/api/notes", (req, res) => {
-        console.log(res);
         fs.readFile("db/db.json", (err, data) => {
             if (err) throw err;
-
-            console.log(data);
             res.json(JSON.parse(data));
         });
     });
 
-    app.post("/api/notes", function(req, res){
+    app.post("/api/notes", function (req, res) {
         let userArray = [];
-       let userNote = req.body;
-       console.log(userNote);
+        let userNote = req.body;
 
-       fs.readFile("db/db.json", (err, data) => {
-           if(err) throw err;
-           userArray = JSON.parse(data);
-           userArray.push(userNote);
-           console.log(userArray);
-           res.json(userNote);     
-           fs.writeFile("db/db.json", JSON.stringify(userArray),(err) => {
-               if(err)throw err;
-               console.log("success");
-             
-           });
-       });
-       
+        fs.readFile("db/db.json", (err, data) => {
+            if (err) throw err;
+            userArray = JSON.parse(data);
+            userArray.push(userNote);
+            console.log(userArray);
+            res.json(userNote);
+            fs.writeFile("db/db.json", JSON.stringify(userArray), (err) => {
+                if (err) throw err;
+                console.log("success");
+
+            });
+        });
+
     });
 }
